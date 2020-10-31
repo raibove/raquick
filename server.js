f1536a468a74eb59ef4cedfdebc5560378c315c5
Shweta Kale <shwetakale144@gmail.com>
2020-10-31 20:19:05 +0530

    Added path in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const passport = require("passport");
+const path = require('path');
 
 //const cors = require("cors");
 
