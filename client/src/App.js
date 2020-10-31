4d04c38e9cae298bb12fe42c86e6f3279a116ed1
Shweta Kale <shwetakale144@gmail.com>
2020-10-31 19:50:38 +0530

    Combined front-end back-end for deployment

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+
+//import Axios from "axios";
+import './App.css';
+
+import ProtectedRoute from './ProtectedRoutes';
+import Landing from './landing/pages/Landing';
+import Auth from './autherization/pages/Auth';
+import AgentProfile from './agent/pages/AgentProfile';
+import Customers from './customer/pages/Customers';
+import Scan from './scan/pages/Scan';
+import DisplayStocks from './stock/pages/DisplayStocks';
+
+/*
+const AgentProfile = React.lazy(() => import('./agent/pages/AgentProfile'));
+const Customers = React.lazy(()=> import('./customer/pages/Customers'));
+const Scan = React.lazy(()=> import('./scan/pages/Scan'));
+const DisplayStocks = React.lazy(()=> import('./stock/pages/DisplayStocks'));
+*/
+
+
+const App = ()=> {
+  const routes = <Switch>
+    <Route path="/" exact>
+      <Landing />
+    </Route>
+    <Route path="/auth" component={Auth} exact />
+    <ProtectedRoute path="/agent" component={AgentProfile} exact/>
+    <ProtectedRoute path="/customers/display" component={Customers} exact />
+    <ProtectedRoute path="/scan" exact component={Scan}/>
+    <ProtectedRoute path="/stocks" component={DisplayStocks} exact />
+    <Redirect to="/"/>
+    </Switch>
+  return (
+    <Router>
+      {routes}
+    </Router>
+  );
+};
+export default App;
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//const cors = require("cors");
+
+const admins = require("./routes/admins");
+//const testApiRoutes = require('./routes/testApi');
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+// Bodyparser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// DB Config
+const db = require("./config/keys").mongoURL;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+//app.use(cors());
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+
+
+// Routes
+app.use("/", admins);
+
+//app.use("/",testApiRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));   
