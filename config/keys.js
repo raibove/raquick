0a178549c589394cc33938d39b3b4b849a0c50ff
Shweta Kale <44832446+raibove@users.noreply.github.com>
2021-06-17 20:13:15 +0530

    Delete keys.js

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURL:
-    'mongodb+srv://ration:ration@cluster0.zo33o.mongodb.net/login_data?retryWrites=true&w=majority',
-  secretOrKey: 'secret',
-  DB_LOCAL: 'mongodb://localhost:27017/ragquick',
-};
